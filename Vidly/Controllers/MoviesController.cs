50fbd6b70dcaa58c90469cc3cbdaf1e3411ac591
Himal Shah <himal13shah@gmail.com>
2019-05-13 15:57:01 -0400

    diffrent possible view results

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,9 +13,21 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie { name = "Avengers" };
-            // return View(movie);
+            return View(movie);
             //return Content("Hey whats up!");    
-            //return new HttpNotFoundResult();
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+           
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            /*
+             * passing parameters to action
+             * parameter can be in URL like /home/edit/1 or in query string like /home/edit?id=1 or form data like id = 1
+             */
+            return Content("ID = " + id);
         }
     }
 }
